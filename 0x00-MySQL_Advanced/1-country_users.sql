68c37e90f8554d4ab1d76a022ccc9ae47a11930b
samlan24 <muchemionline@gmial.com>
2024-10-16 15:33:36 +0000

    1-country_users.sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- A SQL script that creates a table users 
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM( default'US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+)
