7462859ccd683e556a2b7350b03883bb0cc61ef3
samlan24 <muchemionline@gmial.com>
2024-10-16 15:26:31 +0000

    0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
---a SQL script that creates a table users 
-
+-- A SQL script that creates a table users 
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
-);
+)
