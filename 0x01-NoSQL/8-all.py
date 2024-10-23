2ebd78a3e4407187605c81eaff3dca092607f471
samlan24 <muchemionline@gmail.com>
2024-10-23 03:39:24 -0700

    8-all.py

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+
+"""Python function that lists all documents in a collection"""
+
+
+def list_all(mongo_collection):
+    """lists all in a collection"""
+    if (mongo_collection is None):
+        return []
+    return list(mongo_collection.find())
