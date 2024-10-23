0b0d6eda6848a72dfd8e9f2f42d198837b21b75d
samlan24 <muchemionline@gmail.com>
2024-10-23 04:01:23 -0700

    10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Python function that changes all topics of a school
+document based on the name
+"""
+
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school"""
+    return mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
